93cd6e072a9e85423f7a1ff714a59e2d8880e513
TokoTini <129732627+TokoTini@users.noreply.github.com>
2023-05-23 17:04:09 +0200

    maj23/05

--- js.js
+++ js.js
@@ -1,3 +1,9 @@
+// altitude slider
+// altitude slider
+// altitude slider
+// altitude slider
+// altitude slider
+// altitude slider
 const slider = document.getElementById('altitude-slider');
 
 noUiSlider.create(slider, {
@@ -5,10 +11,11 @@ noUiSlider.create(slider, {
   connect: true, // Connect the handles with a colored bar
   step: 100,
   range: {
-    min: 1400,
+    min: 50,
     max: 2900
   }
 });
+
 
 // min price paragraph
 let priceMin = document.querySelector('[data-handle="0"]');
@@ -26,6 +33,7 @@ priceMaxPara.appendChild(pMax);
 slider.noUiSlider.on('change', function (values, handle) {
     const minPrice = values[0];
     const maxPrice = values[1];
+    console.log(minPrice);
 
 });
 
@@ -43,6 +51,56 @@ slider.noUiSlider.on('update', function (values, handle) {
 });
 
 
-100 - 1000
 
-300 - 500
+// km piste slider
+// km piste slider
+// km piste slider
+// km piste slider
+// km piste slider
+// km piste slider
+var connectSlider = document.getElementById('km-piste-slider');
+
+noUiSlider.create(connectSlider, {
+    start: 40,
+    connect: 'lower',
+    step: 50,
+    range: {
+        'min': 50,
+        'max': 1800
+    }
+});
+
+let kmPisteHandle = connectSlider.querySelector('.noUi-touch-area');
+let pKmPiste = document.createElement('p');
+pKmPiste.classList.add('km-piste')
+kmPisteHandle.appendChild(pKmPiste);
+
+// Update the values when the handles are moved
+connectSlider.noUiSlider.on('update', function (value, handle) {
+  let kmPiste = document.querySelector('.km-piste');
+  let str = value.toString();
+  str = str.slice(0, -3);
+  kmPiste.textContent = str;
+});
+
+
+
+
+
+//open / close cards
+//open / close cards
+//open / close cards
+//open / close cards
+//open / close cards
+//open / close cards
+
+let cardBtn = document.querySelectorAll('.open-card');
+console.log(cardBtn);
+
+cardBtn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    let card = btn.closest('.card')
+    let cardContent = card.querySelector('.content')
+    cardContent.classList.toggle('active');
+  })
+})
